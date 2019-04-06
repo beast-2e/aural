3aa9b2ab39f4b20ca9f2d9e4b7e5e8a54996c85b
willy-vvu <willy.vvu@gmail.com>
2019-04-06 15:27:08 -0400

    Added setup script

--- volume.py
+++ volume.py
@@ -1,9 +1,8 @@
-
 #!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
 import alsaaudio
-
 
 GPIO.setmode(GPIO.BCM)
 UP = 21
@@ -23,16 +22,16 @@ while True:
     # Read from pins
     vol_up = GPIO.input(UP)
     vol_down = GPIO.input(DOWN)
-    
+
     # Increment if needed
     if (vol_up and not vol_down):
         new_vol += inc
     elif (vol_down and not vol_up):
         new_vol -= inc
-    
+
     # Make sure the volume is within valid range
     new_vol = min(max(new_vol,0),100)
-        
+
     # Set volume only if it has changed
     if current_vol != new_vol:
         current_vol = new_vol;
